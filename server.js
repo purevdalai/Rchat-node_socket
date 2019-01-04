c5547b1ac60df0ca7aa4a41fe3c01682e1172b41
Purevdalai <purewdalai3@gmail.com>
2019-01-04 08:39:09 +0800

    changes

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ server.listen(8890);
 
 io.on('connection', function(socket) {
     // console.log('new client connected!');
-    var redisClient = redis.createClient();
-    redisClient.subscribe('message');
-    redisClient.on('message', function(channel, message) {
+    var client = redis.createClient();
+    client.subscribe('message');
+    client.on('message', function(channel, message) {
         // console.log('new message in queue => ', channel, message);
         socket.emit(channel, message);
     });
